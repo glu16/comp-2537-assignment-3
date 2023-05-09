262da333dd3042c5c986041520b585c990c53ed9
Gin Lu <glu23@my.bcit.ca>
2023-05-08 21:08:37 -0700

    Added in filter buttons and filter function

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+/* Creates filter buttons for Pokémon */
+document.addEventListener("DOMContentLoaded", function () {
+  const types = [
+    "normal",
+    "fighting",
+    "flying",
+    "poison",
+    "ground",
+    "rock",
+    "bug",
+    "ghost",
+    "steel",
+    "fire",
+    "water",
+    "grass",
+    "electric",
+    "psychic",
+    "ice",
+    "dragon",
+    "dark",
+    "fairy",
+    "unknown",
+    "shadow",
+  ];
+
+  const pokeTypesFilter = document.getElementById("pokeTypesFilter");
+
+  types.forEach((type) => {
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.name = "type";
+    input.value = type;
+    input.id = type;
+    input.className = "typeFilter";
+
+    const label = document.createElement("label");
+    label.htmlFor = type;
+    label.textContent = type;
+
+    pokeTypesFilter.appendChild(input);
+    pokeTypesFilter.appendChild(label);
+  });
+});
+
+/* Creates pagination buttons to display the next 10 Pokémon */
 const numPerPage = 10;
 let currentPage = 1;
 let pokemon = [];
@@ -59,7 +104,7 @@ const paginate = async (currentPage, numPerPage, pokemon) => {
   for (const pokemon of selected_pokemon) {
     const res = await axios.get(pokemon.url);
     $("#pokeCards").append(`
-      <div class="pokeCard card" pokeName=${res.data.name}   >
+      <div class="pokeCard card" pokeName=${res.data.name}>
         <h3>${res.data.name.toUpperCase()}</h3> 
         <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
@@ -70,8 +115,9 @@ const paginate = async (currentPage, numPerPage, pokemon) => {
   }
 };
 
+/* Displays Pokémon from https://pokeapi.co/api/v2/pokemon?offset=0&limit=810 */
 const setup = async () => {
-  // test out poke api using axios here
+  // Test out Poké API using Axios here
 
   $("#pokeCards").empty();
   let response = await axios.get(
@@ -83,8 +129,8 @@ const setup = async () => {
   const numPages = Math.ceil(pokemon.length / numPerPage);
   updatePaginationDiv(currentPage, numPages);
 
-  // pop up modal when clicking on a pokemon card
-  // add event listener to each pokemon card
+  // Pop up modal when clicking on a Pokémon card
+  // Add event listener to each Pokémon card
   $("body").on("click", ".pokeCard", async function (e) {
     const pokemonName = $(this).attr("pokeName");
     // console.log("pokemonName: ", pokemonName);
@@ -131,12 +177,46 @@ const setup = async () => {
         `);
   });
 
-  // add event listener to pagination buttons
+  // Define function to get Pokémon types
+  const getPokemonTypes = async (pokemonName) => {
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    return res.data.types.map((type) => type.type.name);
+  };
+
+  // Add event listener to filter buttons
+  $("body").on("change", ".typeFilter", async function (e) {
+    const selectedTypes = $("input[name='type']:checked")
+      .map(function () {
+        return this.value;
+      })
+      .get();
+
+    let filteredPokemon = await Promise.all(
+      pokemon.map(async function (p) {
+        const pokemonTypes = await getPokemonTypes(p.name);
+        return selectedTypes.every(function (type) {
+          return pokemonTypes.includes(type);
+        })
+          ? p
+          : null;
+      })
+    );
+
+    pokemon = filteredPokemon.filter((p) => p !== null);
+
+    paginate(currentPage, numPerPage, pokemon);
+    const numPages = Math.ceil(pokemon.length / numPerPage);
+    updatePaginationDiv(currentPage, numPages);
+  });
+
+  // Add event listener to pagination buttons
   $("body").on("click", ".numberedButtons", async function (e) {
     currentPage = Number(e.target.value);
     paginate(currentPage, numPerPage, pokemon);
 
-    // update pagination buttons
+    // Update pagination buttons
     updatePaginationDiv(currentPage, numPages);
   });
 };
